9576bc463f8bdcfcffbeba5e77c1f8ca8a89f8e0
Kimberly Rv <elliis.rv@gmail.com>
2023-09-27 17:08:23 -0600

    vite installation

--- movie-challenge/src/App.jsx
+++ movie-challenge/src/App.jsx
@@ -1,21 +1,35 @@
-import './App.css';
-import { Header, BlackContainer, SelectedMoodContainer, MoodContainer } from './components/main-page'
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
-  <Header />
-  <BlackContainer>
-  </BlackContainer>
-  <SelectedMoodContainer color='#E1E1E1' />
-  <div className='ContentContainer' >
-    <MoodContainer color='#F9DB41' onClick={(e)=> console.log(e.target)} />
-    <MoodContainer color='#B5C8FC' />
-    <MoodContainer color='#737B88' />
-  </div>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
